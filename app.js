a029bc693d7f1d05a38c99474b851e2d9b74dd0f
Hunter Karas <44104172+hkaras19@users.noreply.github.com>
2022-09-27 17:45:26 -0400

    final

--- app.js
+++ app.js
@@ -26,15 +26,21 @@ const updatePage = async () => {
 
   // Make API request and get an array of fruit objects
   const fruitsArray = await apiRequest();
-  // console.log(fruitsArray);
 
   // TODO: Use either `map` and/or `filter` to extract some data from the array of fruit objects
   // For example, find "name of all fruits whose sugar > 15",
 
+  const filteredArray = fruitsArray.filter((fruit) => fruit.genus === "Citrus")
+
   // TODO: Create a new HTML element to display your data
 
-  // TODO: Append your new element to the page
+  filteredArray.forEach((fruit) => {
+    const newElement = document.createElement('h1');
+    newElement.innerHTML = fruit.name;
 
+    const existingElement = document.getElementById('fruit-name-div');
+    existingElement.append(newElement);
+  })
 }
 
 // SAMPLE CODE of how to create and append a new HTML element to the page
